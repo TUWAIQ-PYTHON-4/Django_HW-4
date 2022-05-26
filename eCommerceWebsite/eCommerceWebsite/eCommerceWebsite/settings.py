310daf31ad07f132c342c21a5521594c695177c7
dev <dev@test.com>
2022-05-27 01:47:03 +0300

    project eCommerceWebsite well done

--- eCommerceWebsite/eCommerceWebsite/eCommerceWebsite/settings.py
+++ eCommerceWebsite/eCommerceWebsite/eCommerceWebsite/settings.py
@@ -75,10 +75,14 @@ WSGI_APPLICATION = 'eCommerceWebsite.wsgi.application'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+   'default': {
+      'ENGINE': 'django.db.backends.postgresql_psycopg2',
+      'NAME': 'eCommerceDB',
+      'USER': 'postgres',
+      'PASSWORD': '123456',
+      'HOST': 'localhost',
+      'PORT': '5432',
+   }
 }
 
 
@@ -121,6 +125,10 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'eCommerceWebsite/static')
 ]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR , 'media')
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
