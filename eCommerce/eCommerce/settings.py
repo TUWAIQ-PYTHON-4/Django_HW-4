129512888a9bcb9845dc17e6a1849d85778c059d
Emtinan98 <102264151+Emtinan98@users.noreply.github.com>
2022-05-26 09:24:08 +0300

    added

--- eCommerce/eCommerce/settings.py
+++ eCommerce/eCommerce/settings.py
@@ -76,12 +76,8 @@ WSGI_APPLICATION = 'eCommerce.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql',
-        'NAME':'ecommercedb',
-        'USER':'postgres',
-        'PASSWORD':'123456789',
-        'HOST':'localhost'
-
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
